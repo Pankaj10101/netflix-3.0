4d7167e76bbb40d16db795344611e01ba89c4fa6
Pankaj10101 <rkay1048@gmail.com>
2023-06-16 09:56:17 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['rb.gy', 'image.tmdb.org'],
-    },
-}
+  reactStrictMode: true,
+  experimental: {
+    // Specify other experimental options here if needed
+  },
+  images: {
+    domains: ['rb.gy', 'image.tmdb.org'],
+  },
+  // Add the 'export' option to enable static HTML export
+  output: {
+    export: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
